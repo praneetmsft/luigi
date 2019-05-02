178c0eeed8f35b256014a32ae8152e8d2d467eb7
Praneet Singh Solanki <prsol@microsoft.com>
2019-05-02 01:51:08 -0400

    feat: added method to create azure batch pool

--- luigi/contrib/azurebatch.py
+++ luigi/contrib/azurebatch.py
@@ -135,6 +135,37 @@ def get_container_sas_token(block_blob_client, container_name, blob_permissions)
 
     return container_sas_token
 
+def create_pool(batch_service_client, pool_id):
+    """
+    Creates a pool of compute nodes with the specified OS settings.
+    :param batch_service_client: A Batch service client.
+    :type batch_service_client: `azure.batch.BatchServiceClient`
+    :param str pool_id: An ID for the new pool.
+    :param str publisher: Marketplace image publisher
+    :param str offer: Marketplace image offer
+    :param str sku: Marketplace image sku
+    """
+    print('Creating pool [{}]...'.format(pool_id))
+
+    # Create a new pool of Linux compute nodes using an Azure Virtual Machines
+    # Marketplace image. For more information about creating pools of Linux
+    # nodes, see:
+    # https://azure.microsoft.com/documentation/articles/batch-linux-nodes/
+    new_pool = batch.models.PoolAddParameter(
+        id=pool_id,
+        virtual_machine_configuration=batchmodels.VirtualMachineConfiguration(
+            image_reference=batchmodels.ImageReference(
+        	        publisher="Canonical",
+        	        offer="UbuntuServer",
+        	        sku="18.04-LTS",
+        	        version="latest"
+                ),
+        node_agent_sku_id="batch.node.ubuntu 18.04"),
+        vm_size=config._POOL_VM_SIZE,
+        target_dedicated_nodes=config._POOL_NODE_COUNT
+    )
+    batch_service_client.pool.add(new_pool)
+
     def submit_job_and_add_task(self):
         """Submits a job to the Azure Batch service and adds a simple task.
 
