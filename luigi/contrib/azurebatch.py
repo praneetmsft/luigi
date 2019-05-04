9c76f82803311947b2e4bb89936044fd8719b5fa
Praneet Singh Solanki <prsol@microsoft.com>
2019-05-04 13:38:33 -0400

    feat: added print exception method

--- luigi/contrib/azurebatch.py
+++ luigi/contrib/azurebatch.py
@@ -379,13 +379,25 @@ class AzureBatchClient(object):
         """Get the status of all the tasks under one Job"""
         raise NotImplementedError("Yet to be implemented.")
 
-    def print_batch_exception(self):
+    def print_batch_exception(self, batch_exception):
         """
         Prints the contents of the specified Batch exception.
-
         :param batch_exception:
         """
-        raise NotImplementedError("Yet to be implemented.")
+        print("-------------------------------------------")
+        print("Exception encountered:")
+        if (
+            batch_exception.error
+            and batch_exception.error.message
+            and batch_exception.error.message.value
+        ):
+            print(batch_exception.error.message.value)
+            if batch_exception.error.values:
+                print()
+                for mesg in batch_exception.error.values:
+                    print("{}:\t{}".format(mesg.key, mesg.value))
+        print("-------------------------------------------")
+
 
 
 class AzureBatchTask(luigi.Task):
